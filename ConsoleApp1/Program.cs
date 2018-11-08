763c14daa5ea84473b6a0eac8a13c559b63f6c41
ErwinMeulman <erwinmeulman@gmail.com>
2018-11-08 12:49:09 +0100

    VS

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bimangle.ForgeEngine.Dwf.Types.Props.IEntityInfo{"c:\proces.dwf"};
+        }
+    }
+}
